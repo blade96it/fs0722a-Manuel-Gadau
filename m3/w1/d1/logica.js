89c228f4850197e77c33b9793548ec035522292f
blade96it <bladegadau@gmail.com>
2022-12-19 15:50:45 +0100

    esercizio completato

---
+++ m3/w1/d1/logica.js
@@ -0,0 +1,22 @@
+var giocatore1 = 23;
+var giocatore2 = 75;
+function azzeccaIlNumero(primo, secondo) {
+    var numeroCasuale = Math.floor(Math.random() * 101);
+    if (primo == numeroCasuale || secondo == numeroCasuale) {
+        if (primo == numeroCasuale) {
+            console.log("Complimenti! Il Giocatore 1 ha indovinato il numero casuale! Il numero vincente è: " + numeroCasuale);
+        }
+        else {
+            console.log("Complimenti! Il Giocatore 2 ha indovinato il numero casuale! Il numero vincente è: " + numeroCasuale);
+        }
+    }
+    else {
+        if (Math.abs(primo - numeroCasuale) < Math.abs(secondo - numeroCasuale)) { //con Math.abs calcolo la differenza assoluta tra il numero interessato
+            //e il numero casuale, comparando poi i due risultati.
+            console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 1 si è avvicinato di più al numero: " + numeroCasuale);
+        }
+        else {
+            console.log("Nessuno dei due ha azzeccato il numero casuale, ma il Giocatore 2 si è avvicinato di più al numero: " + numeroCasuale);
+        }
+    }
+}
